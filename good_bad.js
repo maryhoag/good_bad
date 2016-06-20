96c6a086ff46c44a682cacd16cdb13c32d3b7761
maryhoag <marymcevoyhoag@gmail.com>
2016-06-20 19:28:09 -0400

    added random insult/compliment

--- good_bad.js
+++ good_bad.js
@@ -15,16 +15,18 @@ var server = http.createServer(handle_request);
 //listen to port1
 server.listen(port1, function() {
 	console.log('Listening to ' + port1);
+	console.log(good[n]);
 });
 //listen to port2
 server.listen(port2, function() {
 	console.log('Listening to ' + port2);
+	console.log(bad[n]);
 });
 
-
+var n = Math.floor(Math.random() * 3);
 //array of insults
 var bad = [
-	'you stuck up, half-witted, scruffy-looking Nerf herder', 'You clinking, clanking, clattering collection of caligenous junk!'
+	'you stuck up, half-witted, scruffy-looking Nerf herder', 'You clinking, clanking, clattering collection of caligenous junk!', 'You\'re a gutless turd.', 'Frankly, my dear, I don\'t give a damn.', ''
 ];
 //array of good things
 var good = [
